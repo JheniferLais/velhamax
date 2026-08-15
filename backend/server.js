96410e3761a01e98c0fa17ddad753948a8c8947d
lais <jheniferlais3@gmail.com>
2026-08-15 08:11:11 -0300

    refactor: apagando comentarios

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*", // Permite que qualquer front-end conecte
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
